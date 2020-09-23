bcc176fcd9be8dc40d1b1c2bd96dc08db6afb1fc
Peter Kovacik <pkovacik2@my.wctc.edu>
2020-09-22 23:25:48 -0500

    convert Northwind website to MVC format

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/client.js
@@ -0,0 +1,3 @@
+﻿$(function () {
+    //alert("red alert");
+});
